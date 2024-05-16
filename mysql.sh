00eccee85320e4e86afc74b95f9ebeb5b03f61ce
prabala <radhakrishnaprabala8@gmail.com>
2024-05-16 18:20:44 +0530

    mysql.sh-ver1-echo

--- mysql.sh
+++ mysql.sh
@@ -1,6 +1,13 @@
+echo -e "\e[32m >>>>>>>disable mysql module<<<<<<<<< \e[0m"
 dnf module disable mysql -y
-cp mysql.repo /etc/yum.repos.d/mysql.repo
+
+echo -e "\e[32m >>>>>>>cp mysql repo<<<<<<<<< \e[0m"
+cp /home/centos/roboshop-72/mysql.repo /etc/yum.repos.d/mysql.repo
+
+echo -e "\e[32m >>>>>>>install mysql <<<<<<<<< \e[0m"
 dnf install mysql-community-server -y
 systemctl enable mysqld
-systemctl start mysqld
+systemctl restart mysqld
+
+echo -e "\e[32m >>>>>>>user creation mysql <<<<<<<<< \e[0m"
 mysql_secure_installation --set-root-pass RoboShop@1
