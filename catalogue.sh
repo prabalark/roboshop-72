c11e55aa4c721e85782a15f66c6da024f8c979f5
prabala <radhakrishnaprabala8@gmail.com>
2024-05-16 15:01:38 +0530

    catalogue.sh-ver-echo& rmadded2

--- catalogue.sh
+++ catalogue.sh
@@ -18,7 +18,7 @@ echo -e "\e[31m >>>>>>> install systemd<<<<<<< \e[0m"
 cp /home/centos/roboshop-72/catalogue.systemd /etc/systemd/system/catalogue.service
 systemctl daemon-reload
 systemctl enable catalogue
-systemctl start catalogue
+systemctl restart catalogue
 
 echo -e "\e[31m >>>>>>> install mongodb <<<<<<< \e[0m"
 cp /home/centos/roboshop-72/mongo.repo /etc/yum.repos.d/mongo.repo
