2d2b09cfd7d04f120e7bad118828491ae1c7324a
prabala <radhakrishnaprabala8@gmail.com>
2024-05-16 19:04:06 +0530

    Dispatch.sh-ver1-echo

--- Dispatch.sh
+++ Dispatch.sh
@@ -1,14 +1,26 @@
+echo -e " \e[32m >>>>> install golang <<<<< \e[0m "
 dnf install golang -y
+
+echo -e " \e[32m >>>>> downlaod app <<<<< \e[0m "
 useradd roboshop
 mkdir /app
 curl -L -o /tmp/dispatch.zip https://roboshop-artifacts.s3.amazonaws.com/dispatch.zip
+
+echo -e " \e[32m >>>>> unzip app <<<<< \e[0m "
 cd /app
 unzip /tmp/dispatch.zip
+
+echo -e " \e[32m >>>>> install library <<<<< \e[0m "
 cd /app
 go mod init dispatch
 go get
 go build
+
+echo -e " \e[32m >>>>> install systemd <<<<< \e[0m "
 cp dispatch.systemd  /etc/systemd/system/dispatch.service
 systemctl daemon-reload
 systemctl enable dispatch
-systemctl start dispatch
+systemctl restart dispatch
+
+
+
