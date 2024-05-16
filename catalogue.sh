584834083a13c320b2df43cb4c583089ffbecfb1
prabala <radhakrishnaprabala8@gmail.com>
2024-05-16 15:16:56 +0530

    catalogue.sh-ver4

--- catalogue.sh
+++ catalogue.sh
@@ -3,13 +3,16 @@ dnf module disable nodejs -y
 dnf module enable nodejs:18 -y
 dnf install nodejs -y
 
-echo -e "\e[31m >>>>>>>un-zip catalogue<<<<<<< \e[0m"
+echo -e "\e[31m >>>>>>>create /app <<<<<< \e[0m"
 useradd roboshop
 rm -rf /app # bcz re-run of code some time through error
 mkdir /app
 
+echo -e "\e[31m >>>>>>>downlaod catalogue<<<<<<< \e[0m"
 curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
 cd /app
+
+echo -e "\e[31m >>>>>>>unzip catalogue<<<<<<< \e[0m"
 unzip /tmp/catalogue.zip
 
 echo -e "\e[31m >>>>>>>library install<<<<<<< \e[0m"
@@ -17,7 +20,7 @@ cd /app
 npm install
 
 echo -e "\e[31m >>>>>>> install systemd<<<<<<< \e[0m"
-cp /home/centos/roboshop-72/catalogue.systemd /etc/systemd/system/catalogue.service
+cp /home/centos/roboshop-72/catalogue.systemd  /etc/systemd/system/catalogue.service
 systemctl daemon-reload
 systemctl enable catalogue
 systemctl restart catalogue
