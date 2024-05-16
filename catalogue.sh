d3529865858ca286339da29cda7d03caffb8af36
prabala <radhakrishnaprabala8@gmail.com>
2024-05-16 15:08:03 +0530

    catalogue.sh-ver-echo3

--- catalogue.sh
+++ catalogue.sh
@@ -2,10 +2,12 @@ echo -e "\e[31m >>>>>>> install nodejs <<<<<<< \e[0m"
 dnf module disable nodejs -y
 dnf module enable nodejs:18 -y
 dnf install nodejs -y
+
 echo -e "\e[31m >>>>>>>un-zip catalogue<<<<<<< \e[0m"
 useradd roboshop
 rm -rf /app # bcz re-run of code some time through error
 mkdir /app
+
 curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
 cd /app
 unzip /tmp/catalogue.zip
@@ -20,9 +22,13 @@ systemctl daemon-reload
 systemctl enable catalogue
 systemctl restart catalogue
 
-echo -e "\e[31m >>>>>>> install mongodb <<<<<<< \e[0m"
+echo -e "\e[31m >>>>>>> copy mongodb repo <<<<<<< \e[0m"
 cp /home/centos/roboshop-72/mongo.repo /etc/yum.repos.d/mongo.repo
+
+echo -e "\e[31m >>>>>>> install mongodb client <<<<<<< \e[0m"
 dnf install mongodb-org-shell -y
+
+echo -e "\e[31m >>>>>>> load schema <<<<<<< \e[0m"
 mongo --host mangodb.devops72bat.online </app/schema/catalogue.js
 
 
