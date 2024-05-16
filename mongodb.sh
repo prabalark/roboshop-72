8a69e7ebac3e989b5876eb845595c5dba4238d8f
prabala <radhakrishnaprabala8@gmail.com>
2024-05-16 08:22:00 +0530

    mongodb.sh-ver-cp& updatelisten2

--- mongodb.sh
+++ mongodb.sh
@@ -2,5 +2,5 @@ cp /home/centos/roboshop-72/mongo.repo /etc/yum.repos.d/mongo.repo
 dnf install mongodb-org -y
 sed -i -e 's|127.0.0.1|0.0.0.0|' /etc/mongod.conf
 systemctl enable mongod
-systemctl start mongod
+systemctl restart mongod
 #Update listen address from 127.0.0.1 to 0.0.0.0 in /etc/mongod.conf
