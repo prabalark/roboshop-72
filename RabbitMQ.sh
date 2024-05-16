e2ff38ae8df04e03fd035a5ed6d2c41fd6d1f251
prabala <radhakrishnaprabala8@gmail.com>
2024-05-16 18:39:19 +0530

    RabbitMQ.sh-ver1-echo

--- RabbitMQ.sh
+++ RabbitMQ.sh
@@ -1,7 +1,13 @@
+echo -e "\e[32m >>>>> download rabbitmq <<<<< \e[0m"
 curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | bash
 curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | bash
+
+echo -e "\e[32m >>>>> install rabbitmq <<<<< \e[0m"
 dnf install rabbitmq-server -y
-systemctl enable rabbitmq-server
-systemctl start rabbitmq-server
+
+echo -e "\e[32m >>>>> start rabbitmq <<<<< \e[0m"
+systemctl restart rabbitmq-server
+
+echo -e "\e[32m >>>>> adduser rabbitmq <<<<< \e[0m"
 rabbitmqctl add_user roboshop roboshop123
 rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"
