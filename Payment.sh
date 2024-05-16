abf908572361cec6b2d2827f82dedb9d6a532abb
prabala <radhakrishnaprabala8@gmail.com>
2024-05-16 18:46:31 +0530

    payment.sh-ver1-echo

--- Payment.sh
+++ Payment.sh
@@ -1,12 +1,21 @@
+echo -e "\e[32m  >>>>>>>>>> install python <<<<<<<<< \e[0m"
 dnf install python36 gcc python3-devel -y
+
+echo -e "\e[32m  >>>>>>>>>> download app <<<<<<<< \e[0m"
 useradd roboshop
 mkdir /app
 curl -L -o /tmp/payment.zip https://roboshop-artifacts.s3.amazonaws.com/payment.zip
+
+echo -e "\e[32m  >>>>>>>>>> unzip app <<<<<<<< \e[0m"
 cd /app
 unzip /tmp/payment.zip
+
+echo -e "\e[32m  >>>>>>>>>> install library <<<<<<<< \e[0m"
 cd /app
 pip3.6 install -r requirements.txt
-cp payment.systemd  /etc/systemd/system/payment.service
+
+echo -e "\e[32m  >>>>>>>>>> install systemd <<<<<<<< \e[0m"
+cp /home/centos/roboshop-72/payment.systemd  /etc/systemd/system/payment.service
 systemctl daemon-reload
 systemctl enable payment
-systemctl start payment
+systemctl restart payment
