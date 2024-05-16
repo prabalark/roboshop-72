ba032bb35e1d6bd018d9b9023af5a03cd2383836
prabala <radhakrishnaprabala8@gmail.com>
2024-05-16 17:38:11 +0530

    redis.sh-ver-echo

--- redis.sh
+++ redis.sh
@@ -1,6 +1,12 @@
+echo -e "\e[32m >>>>>>>>>install repo<<<<<<<<<< \e[0m"
 dnf install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y
 dnf module enable redis:remi-6.2 -y
 dnf install redis -y
+
+echo -e "\e[32m >>>>>>>>>install update listen <<<<<<<<<< \e[0m"
 #Update listen address from 127.0.0.1 to 0.0.0.0 in /etc/redis.conf
+sed -i -e 's|127.0.0.1|0.0.0.0|' /etc/redis.conf
+
+echo -e "\e[32m >>>>>>>>>install start redis <<<<<<<<<< \e[0m"
 systemctl enable redis
-systemctl start redis
+systemctl retart redis
