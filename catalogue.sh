861e4b3be2763a05d14d6e333cf63321baa0c436
prabala <radhakrishnaprabala8@gmail.com>
2024-05-16 14:52:32 +0530

    catalogue.sh-ver-echo& rmadded1

--- catalogue.sh
+++ catalogue.sh
@@ -4,12 +4,14 @@ dnf module enable nodejs:18 -y
 dnf install nodejs -y
 echo -e "\e[31m >>>>>>>un-zip catalogue<<<<<<< \e[0m"
 useradd roboshop
+rm -rf /app # bcz re-run of code some time through error
 mkdir /app
 curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
 cd /app
 unzip /tmp/catalogue.zip
-cd /app
+
 echo -e "\e[31m >>>>>>>library install<<<<<<< \e[0m"
+cd /app
 npm install
 
 echo -e "\e[31m >>>>>>> install systemd<<<<<<< \e[0m"
