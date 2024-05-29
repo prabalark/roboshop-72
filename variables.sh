6cdf3d21ca9a491199aa6d8a224e0175def3515b
prabala <radhakrishnaprabala8@gmail.com>
2024-05-29 22:28:11 +0530

    variables.sh ver-spva21

--- variables.sh
+++ variables.sh
@@ -3,5 +3,13 @@
 echo $0
 dirname
 echo dirname
-echo dirname $0
+echo dirname "$0"
 echo dirname$0
+
+script=$(realpath "$0")
+script_path=$(dirname "$script")
+source  ${script_path}/common.sh
+
+echo   ${script_path}
+
+exit
