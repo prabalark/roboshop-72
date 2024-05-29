e542452f94ffce23ecbfff30f36ff77625ca54db
prabala <radhakrishnaprabala8@gmail.com>
2024-05-29 22:22:47 +0530

    catalogue.sh.sh ver-spva21

--- catalogue.sh
+++ catalogue.sh
@@ -2,8 +2,6 @@ script=$(realpath "$0")
 script_path=$(dirname "$script")
 source  ${script_path}/common.sh
 
-exit
-
 echo -e "\e[31m >>>>>>> install nodejs <<<<<<< \e[0m"
 dnf module disable nodejs -y
 dnf module enable nodejs:18 -y
