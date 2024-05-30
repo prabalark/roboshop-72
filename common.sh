8a3c5553a3023327e6067e6c2449a3836da05862
prabala <radhakrishnaprabala8@gmail.com>
2024-05-30 22:47:58 +0530

    common.sh funver1-nodejs

--- common.sh
+++ common.sh
@@ -1,28 +1,30 @@
 username=roboshop
 
+print_head(){echo -e "\e[31m >>>>>>> $1 <<<<<<< \e[0m"}
+
 func_nodejs(){
-echo -e "\e[31m >>>>>>> install nodejs <<<<<<< \e[0m"
+print_head "install nodejs"
 dnf module disable nodejs -y
 dnf module enable nodejs:18 -y
 dnf install nodejs -y
 
-echo -e "\e[31m >>>>>>>create /app <<<<<< \e[0m"
+print_head "create /app"
 useradd ${username}
 rm -rf /app # bcz re-run of code some time through error
 mkdir /app
 
-echo -e "\e[31m >>>>>>>downlaod catalogue<<<<<<< \e[0m"
+print_head "downlaod catalogue"
 curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
 cd /app
 
-echo -e "\e[31m >>>>>>>unzip catalogue<<<<<<< \e[0m"
+print_head "unzip catalogue"
 unzip /tmp/${component}.zip
 
-echo -e "\e[32m >>>>>>>library install<<<<<<< \e[0m"
+print_head "library install"
 cd /app
 npm install
 
-echo -e "\e[32m >>>>>>> install systemd<<<<<<< \e[0m"
+print_head "install systemd"
 cp $script_path/${component}.systemd  /etc/systemd/system/catalogue.service
 systemctl daemon-reload
 systemctl enable catalogue
