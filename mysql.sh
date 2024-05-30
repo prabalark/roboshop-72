95f4d756192d1d8a4500afb6fceeff4f499466e2
prabala <radhakrishnaprabala8@gmail.com>
2024-05-30 10:24:59 +0530

    mysql.sh passwdcvr2

--- mysql.sh
+++ mysql.sh
@@ -1,10 +1,13 @@
+script=$(realpath "$0")
+script_path=$(dirname "$script")
+source ${script_path}/common.sh
 mysqlpassword=$1
 
 echo -e "\e[32m >>>>>>>disable mysql module<<<<<<<<< \e[0m"
 dnf module disable mysql -y
 
 echo -e "\e[32m >>>>>>>cp mysql repo<<<<<<<<< \e[0m"
-cp /home/centos/roboshop-72/mysql.repo /etc/yum.repos.d/mysql.repo
+cp ${script_path}/mysql.repo /etc/yum.repos.d/mysql.repo
 
 echo -e "\e[32m >>>>>>>install mysql <<<<<<<<< \e[0m"
 dnf install mysql-community-server -y
