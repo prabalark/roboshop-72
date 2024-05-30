8b87a4aa0400c0bfe3f3ffe7d9e66ddf29054f21
prabala <radhakrishnaprabala8@gmail.com>
2024-05-30 23:09:41 +0530

    user.sh funver1-nodejs

--- user.sh
+++ user.sh
@@ -2,40 +2,16 @@ script=$(realpath "$0")
 script_path=$(dirname "$script")
 source ${script_path}/common.sh
 
-echo -e "\e[31m >>>>>>> install nodejs <<<<<<< \e[0m"
-dnf module disable nodejs -y
-dnf module enable nodejs:18 -y
-dnf install nodejs -y
-
-echo -e "\e[31m >>>>>>>create /app <<<<<< \e[0m"
-useradd ${username}
-rm -rf /app # bcz re-run of code some time through error
-mkdir /app
-
-echo -e "\e[31m >>>>>>>downlaod catalogue<<<<<<< \e[0m"
-curl -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user.zip
-cd /app
-
-echo -e "\e[31m >>>>>>>unzip catalogue<<<<<<< \e[0m"
-unzip /tmp/user.zip
-
-echo -e "\e[32m >>>>>>>library install<<<<<<< \e[0m"
-cd /app
-npm install
-
-echo -e "\e[32m >>>>>>> install systemd<<<<<<< \e[0m"
-cp $script_path/user.systemd  /etc/systemd/system/user.service
-systemctl daemon-reload
-systemctl enable user
-systemctl restart user
+component=user
+func_nodejs
 
 echo -e "\e[32m >>>>>>> copy mongodb repo <<<<<<< \e[0m"
-cp /home/centos/roboshop-72/mongo.repo /etc/yum.repos.d/mongo.repo
+cp $script_path/mongo.repo /etc/yum.repos.d/mongo.repo
 
 echo -e "\e[32m >>>>>>> install mongodb client <<<<<< \e[0m"
 dnf install mongodb-org-shell -y
 
 echo -e "\e[32m >>>>>>> load schema <<<<<< \e[0m"
-mongo --host mongodb.devops72bat.online </app/schema/user.js
+mongo --host mongodb.devops72bat.online </app/schema/${component}.js
 
 
