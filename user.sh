23e60efe06c5598d646f0af9437c3119a5909066
prabala <radhakrishnaprabala8@gmail.com>
2024-05-31 22:07:44 +0530

    user.sh funver1-nodejs

--- user.sh
+++ user.sh
@@ -3,6 +3,36 @@ script_path=$(dirname "$script")
 source ${script_path}/common.sh
 
 component=user
+
+func_nodejs(){
+print_head "install nodejs"
+dnf module disable nodejs -y
+dnf module enable nodejs:18 -y
+dnf install nodejs -y
+
+print_head "create /app"
+useradd ${username}
+rm -rf /app # bcz re-run of code some time through error
+mkdir /app
+
+print_head "downlaod catalogue"
+curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
+cd /app
+
+print_head "unzip catalogue"
+unzip /tmp/${component}.zip
+
+print_head "library install"
+cd /app
+npm install
+
+print_head "install systemd"
+cp $script_path/${component}.systemd  /etc/systemd/system/catalogue.service
+systemctl daemon-reload
+systemctl enable catalogue
+systemctl restart catalogue
+}
+
 func_nodejs
 
 echo -e "\e[32m >>>>>>> copy mongodb repo <<<<<<< \e[0m"
