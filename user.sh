647e763ab6223926ce26e846a33b1a461717ff1f
prabala <radhakrishnaprabala8@gmail.com>
2024-05-31 22:56:28 +0530

    user.sh funver1-nodejs1

--- user.sh
+++ user.sh
@@ -7,7 +7,7 @@ component=user
 func_nodejs
 
 echo -e "\e[32m >>>>>>> copy mongodb repo <<<<<<< \e[0m"
-cp /home/centos/roboshop-72/mongo.repo /etc/yum.repos.d/mongo.repo
+cp ${script_path}/mongo.repo /etc/yum.repos.d/mongo.repo
 
 echo -e "\e[32m >>>>>>> install mongodb client <<<<<<< \e[0m"
 dnf install mongodb-org-shell -y
