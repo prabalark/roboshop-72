dc655f0613a0aaa2083bac837549261431ef34a7
prabala <radhakrishnaprabala8@gmail.com>
2024-06-01 15:56:46 +0530

    mysql.sh funver2

--- mysql.sh
+++ mysql.sh
@@ -1,7 +1,11 @@
 script=$(realpath "$0")
 script_path=$(dirname "$script")
 source ${script_path}/common.sh
-mysqlpassword=$1
+
+ if [ -z ${mysqlpassword} ] ; then
+   echo mysqlpassword is not given
+     exit
+    fi
 
 echo -e "\e[32m >>>>>>>disable mysql module<<<<<<<<< \e[0m"
 dnf module disable mysql -y
