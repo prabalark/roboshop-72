e325e3003db5f8cfed541fe9e0fd6e8362112d7b
prabala <radhakrishnaprabala8@gmail.com>
2024-06-01 15:41:02 +0530

    shipping.sh funver2

--- Shipping.sh
+++ Shipping.sh
@@ -3,6 +3,8 @@ script_path=$(dirname "$script")
 source ${script_path}/common.sh
 mysqlpassword=$1
 
+component_shp = shipping
+func_shipping(){
 echo -e " \e[31m >>>>> install maven <<<<< \e[0m"
 dnf install maven -y
 
@@ -11,12 +13,12 @@ useradd ${username}
 rm -rf /app # bcz re-run of code some time through error
 mkdir /app
 
-echo -e " \e[31m >>>>> downlaod shipping <<<<< \e[0m"
-curl -L -o /tmp/shipping.zip https://roboshop-artifacts.s3.amazonaws.com/shipping.zip
+echo -e " \e[31m >>>>> downlaod ${component_shp} <<<<< \e[0m"
+curl -L -o /tmp/${component_shp}.zip https://roboshop-artifacts.s3.amazonaws.com/${component_shp}.zip
 cd /app
 
-echo -e " \e[31m >>>>> unzip shipping <<<<< \e[0m"
-unzip /tmp/shipping.zip
+echo -e " \e[31m >>>>> unzip ${component_shp} <<<<< \e[0m"
+unzip /tmp/${component_shp}.zip
 
 echo -e " \e[31m >>>>> install library <<<<< \e[0m"
 cd /app
@@ -28,12 +30,15 @@ dnf install mysql -y
 
 #RoboShop@1 --password as input
 echo -e " \e[31m >>>>> load schema mysql <<<<< \e[0m"
-mysql -h mysql.devops72bat.online -uroot -p${mysqlpassword} < /app/schema/shipping.sql
+mysql -h mysql.devops72bat.online -uroot -p${mysqlpassword} < /app/schema/${component_shp}.sql
 
 echo -e " \e[31m >>>>> install systemd <<<<< \e[0m"
-cp $script_path/shipping.systemd /etc/systemd/system/shipping.service
+cp $script_path/${component_shp}.systemd /etc/systemd/system/${component_shp}.service
 
 echo -e " \e[31m >>>>> start mysql <<<<< \e[0m"
 systemctl daemon-reload
-systemctl enable shipping
-systemctl restart shipping
+systemctl enable ${component_shp}
+systemctl restart ${component_shp}
+}
+
+func_shipping
