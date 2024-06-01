f328e5d23fa5f3404b6254eb77e94bea732a0e79
prabala <radhakrishnaprabala8@gmail.com>
2024-06-01 12:03:43 +0530

    catalogue.sh funver1

--- catalogue.sh
+++ catalogue.sh
@@ -5,13 +5,6 @@ source ${script_path}/common.sh
 component=catalogue
  func_nodejs
 
-echo -e "\e[32m >>>>>>> copy mongodb repo <<<<<<< \e[0m"
-cp ${script_path}/mongo.repo /etc/yum.repos.d/mongo.repo
 
-echo -e "\e[32m >>>>>>> install mongodb client <<<<<<< \e[0m"
-dnf install mongodb-org-shell -y
-
-echo -e "\e[32m >>>>>>> load schema <<<<<< \e[0m"
-mongo --host mongodb.devops72bat.online </app/schema/${component}.js
 
 
