5ba31760cb3ed09a51645fff72b18766730f4e53
prabala <radhakrishnaprabala8@gmail.com>
2024-06-01 12:05:24 +0530

    user.sh funver1

--- user.sh
+++ user.sh
@@ -4,12 +4,3 @@ source ${script_path}/common.sh
 
 component=user
 func_nodejs
-
-echo -e "\e[32m >>>>>>> copy mongodb repo <<<<<<< \e[0m"
-cp ${script_path}/mongo.repo /etc/yum.repos.d/mongo.repo
-
-echo -e "\e[32m >>>>>>> install mongodb client <<<<<<< \e[0m"
-dnf install mongodb-org-shell -y
-
-echo -e "\e[32m >>>>>>> load schema <<<<<< \e[0m"
-mongo --host mongodb.devops72bat.online </app/schema/${component}.js
