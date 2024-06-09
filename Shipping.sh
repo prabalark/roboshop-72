23798e59db1d0e1fd4e56cbf2947c65a1f9fc56a
prabala <radhakrishnaprabala8@gmail.com>
2024-06-09 23:27:06 +0530

    shipping.sh funver1

--- Shipping.sh
+++ Shipping.sh
@@ -9,41 +9,55 @@ if [ -z "${mysqlpassword}" ]; then
 fi
 
 component_shp=shipping
+
 func_shipping(){
 echo -e " \e[31m >>>>> install maven <<<<< \e[0m"
-dnf install maven -y
+dnf install maven -y &>>${log_path}
+func_exit $?
 
 echo -e "\e[31m >>>>>>>create /app <<<<<< \e[0m"
-useradd ${username}
-rm -rf /app # bcz re-run of code some time through error
-mkdir /app
+useradd ${username} &>>${log_path}
+func_exit $?
+rm -rf /app  &>>${log_path}
+func_exit $? # bcz re-run of code some time through error
+mkdir /app  &>>${log_path}
+func_exit $?
 
 echo -e " \e[31m >>>>> downlaod ${component_shp} <<<<< \e[0m"
-curl -L -o /tmp/${component_shp}.zip https://roboshop-artifacts.s3.amazonaws.com/${component_shp}.zip
-cd /app
+curl -L -o /tmp/${component_shp}.zip https://roboshop-artifacts.s3.amazonaws.com/${component_shp}.zip &>>${log_path}
+func_exit $?
+cd /app &>>${log_path}
 
 echo -e " \e[31m >>>>> unzip ${component_shp} <<<<< \e[0m"
-unzip /tmp/${component_shp}.zip
+unzip /tmp/${component_shp}.zip &>>${log_path}
+func_exit $?
 
 echo -e " \e[31m >>>>> install library <<<<< \e[0m"
-cd /app
-mvn clean package
-mv target/shipping-1.0.jar shipping.jar
+cd /app &>>${log_path}
+mvn clean package &>>${log_path}
+func_exit $?
+mv target/shipping-1.0.jar shipping.jar &>>${log_path}
+func_exit $?
 
 echo -e " \e[31m >>>>> install mysql <<<<< \e[0m"
-dnf install mysql -y
+dnf install mysql -y &>>${log_path}
+func_exit $?
 
 #RoboShop@1 --password as input
 echo -e " \e[31m >>>>> load schema mysql <<<<< \e[0m"
-mysql -h mysql.devops72bat.online -uroot -p${mysqlpassword} < /app/schema/${component_shp}.sql
+mysql -h mysql.devops72bat.online -uroot -p${mysqlpassword} < /app/schema/${component_shp}.sql &>>${log_path}
+func_exit $?
 
 echo -e " \e[31m >>>>> install systemd <<<<< \e[0m"
-cp $script_path/${component_shp}.systemd /etc/systemd/system/${component_shp}.service
+cp $script_path/${component_shp}.systemd /etc/systemd/system/${component_shp}.service &>>${log_path}
+func_exit $?
 
 echo -e " \e[31m >>>>> start mysql <<<<< \e[0m"
-systemctl daemon-reload
-systemctl enable ${component_shp}
-systemctl restart ${component_shp}
+systemctl daemon-reload &>>${log_path}
+func_exit $?
+systemctl enable ${component_shp}  &>>${log_path}
+systemctl restart ${component_shp} &>>${log_path}
+func_exit $?
 }
 
 func_shipping
