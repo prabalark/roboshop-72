1dd0495146ca2cb5fcaf4fbdd9016682416b55e7
prabala <radhakrishnaprabala8@gmail.com>
2024-06-09 23:17:43 +0530

    mysql.sh funver1

--- mysql.sh
+++ mysql.sh
@@ -9,16 +9,22 @@ mysqlpassword=$1
  fi
 
 echo -e "\e[32m >>>>>>>disable mysql module<<<<<<<<< \e[0m"
-dnf module disable mysql -y
+dnf module disable mysql -y &>>${log_path}
+func_exit $?
 
 echo -e "\e[32m >>>>>>>cp mysql repo<<<<<<<<< \e[0m"
-cp ${script_path}/mysql.repo /etc/yum.repos.d/mysql.repo
+cp ${script_path}/mysql.repo /etc/yum.repos.d/mysql.repo &>>${log_path}
+func_exit $?
 
 echo -e "\e[32m >>>>>>>install mysql <<<<<<<<< \e[0m"
-dnf install mysql-community-server -y
-systemctl enable mysqld
-systemctl restart mysqld
+dnf install mysql-community-server -y &>>${log_path}
+func_exit $?
+systemctl enable mysqld &>>${log_path}
+func_exit $?
+systemctl restart mysqld &>>${log_path}
+func_exit $?
 
 #RoboShop@1
 echo -e "\e[32m >>>>>>>user creation mysql <<<<<<<<< \e[0m"
-mysql_secure_installation --set-root-pass ${mysqlpassword}
+mysql_secure_installation --set-root-pass ${mysqlpassword} &>>${log_path}
+func_exit $?
