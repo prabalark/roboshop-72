746906afe7840fb50a590fa07884f1608e3385f3
prabala <radhakrishnaprabala8@gmail.com>
2024-06-09 22:19:16 +0530

    frontend.sh funver1

--- frontend.sh
+++ frontend.sh
@@ -1,15 +1,23 @@
 echo -e "\e[31m >>>>>>> install nginx <<<<<<< \e[0m"
-dnf install nginx -y
-systemctl enable nginx
-systemctl start nginx
+dnf install nginx -y &>>${log_path}
+func_exit $?
+systemctl enable nginx &>>${log_path}
+systemctl start nginx &>>${log_path}
+func_exit $?
 
 echo -e "\e[31m >>>>>>> unzip frontend<<<<<<< \e[0m"
-rm -rf /usr/share/nginx/html/*
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
-cd /usr/share/nginx/html
-unzip /tmp/frontend.zip
+rm -rf /usr/share/nginx/html/* &>>${log_path}
+func_exit $?
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>${log_path}
+func_exit $?
+cd /usr/share/nginx/html &>>${log_path}
+func_exit $?
+unzip /tmp/frontend.zip &>>${log_path}
+func_exit $?
 
 echo -e "\e[31m >>>>>>> reverse-proxy<<<<<<< \e[0m"
-cp /home/centos/roboshop-72/reverse-proxy-ver-1  /etc/nginx/default.d/roboshop.conf
-systemctl enable nginx
-systemctl restart nginx
+cp /home/centos/roboshop-72/reverse-proxy-ver-1  /etc/nginx/default.d/roboshop.conf &>>${log_path}
+func_exit $?
+systemctl enable nginx &>>${log_path}
+systemctl restart nginx &>>${log_path}
+func_exit $?
