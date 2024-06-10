f079a109332ff86d58b3f87fe2ce1c5f4febe1b6
prabala <radhakrishnaprabala8@gmail.com>
2024-06-10 08:57:17 +0530

    payment.sh funver1

--- Payment.sh
+++ Payment.sh
@@ -11,40 +11,40 @@ fi
 component_pay=payment
 
 func_payment(){
-echo -e "\e[32m  >>>>>>>>>> install python <<<<<<<<< \e[0m"
+print_head  "install python"   
 dnf install python36 gcc python3-devel -y &>>${log_path}
 func_exit $?
 
-echo -e "\e[32m  >>>>>>>>>> useradd app <<<<<<<< \e[0m"
+print_head  "useradd app"   
 useradd ${username} &>>${log_path}
 func_exit $?
 
-echo -e "\e[32m  >>>>>>>>>> create app <<<<<<<< \e[0m"
+print_head  "create app"   
 rm -rf /app &>>${log_path}
 mkdir /app &>>${log_path}
 func_exit $?
 
-echo -e "\e[32m  >>>>>>>>>> download ${component_pay}.zip <<<<<<<< \e[0m"
+print_head  "download ${component_pay}.zip"   
 curl -L -o /tmp/${component_pay}.zip https://roboshop-artifacts.s3.amazonaws.com/${component_pay}.zip &>>${log_path}
 func_exit $?
 
-echo -e "\e[32m  >>>>>>>>>> unzip app <<<<<<<< \e[0m"
+print_head  "unzip app"   
 cd /app &>>${log_path}
 unzip /tmp/${component_pay}.zip &>>${log_path}
 func_exit $?
 
-echo -e "\e[32m  >>>>>>>>>> install library <<<<<<<< \e[0m"
+print_head  "install library"   
 cd /app &>>${log_path}
 pip3.6 install -r requirements.txt &>>${log_path}
 func_exit $?
 
 #roboshop123
-echo -e "\e[32m  >>>>>>>>>> install systemd <<<<<<<< \e[0m"
+print_head  "install systemd"   
 sed -i -e "s|rabbitm1_lg_password2|${rabbitm1_lg_password}|" ${script_path}/${component_pay}.systemd &>>${log_path}
 func_exit $?
 cp ${script_path}/${component_pay}.systemd  /etc/systemd/system/${component_pay}.service &>>${log_path}
 
-echo -e "\e[32m  >>>>>>>>>> start ${component_pay} systemd <<<<<<<< \e[0m"
+print_head  "start ${component_pay} systemd"   
 systemctl daemon-reload  &>>${log_path}
 func_exit $?
 systemctl enable ${component_pay} &>>${log_path}
