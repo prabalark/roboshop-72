e2a63482e78d40d20f4650bdc212b1f2f05c2c13
prabala <radhakrishnaprabala8@gmail.com>
2024-06-10 08:47:37 +0530

    shipping.sh funver1

--- Shipping.sh
+++ Shipping.sh
@@ -11,11 +11,11 @@ fi
 component_shp=shipping
 
 func_shipping(){
-echo -e " \e[31m >>>>> install maven <<<<< \e[0m"
+print_head  "install maven" 
 dnf install maven -y &>>${log_path}
 func_exit $?
 
-echo -e "\e[31m >>>>>>>create /app <<<<<< \e[0m"
+print_head "create /app"
 useradd ${username} &>>${log_path}
 func_exit $?
 rm -rf /app  &>>${log_path}
@@ -23,36 +23,36 @@ func_exit $? # bcz re-run of code some time through error
 mkdir /app  &>>${log_path}
 func_exit $?
 
-echo -e " \e[31m >>>>> downlaod ${component_shp} <<<<< \e[0m"
+print_head  "downlaod ${component_shp}" 
 curl -L -o /tmp/${component_shp}.zip https://roboshop-artifacts.s3.amazonaws.com/${component_shp}.zip &>>${log_path}
 func_exit $?
 cd /app &>>${log_path}
 
-echo -e " \e[31m >>>>> unzip ${component_shp} <<<<< \e[0m"
+print_head  "unzip ${component_shp}" 
 unzip /tmp/${component_shp}.zip &>>${log_path}
 func_exit $?
 
-echo -e " \e[31m >>>>> install library <<<<< \e[0m"
+print_head  "install library" 
 cd /app &>>${log_path}
 mvn clean package &>>${log_path}
 func_exit $?
 mv target/shipping-1.0.jar shipping.jar &>>${log_path}
 func_exit $?
 
-echo -e " \e[31m >>>>> install mysql <<<<< \e[0m"
+print_head  "install mysql" 
 dnf install mysql -y &>>${log_path}
 func_exit $?
 
 #RoboShop@1 --password as input
-echo -e " \e[31m >>>>> load schema mysql <<<<< \e[0m"
+print_head  "load schema mysql" 
 mysql -h mysql.devops72bat.online -uroot -p${mysqlpassword} < /app/schema/${component_shp}.sql &>>${log_path}
 func_exit $?
 
-echo -e " \e[31m >>>>> install systemd <<<<< \e[0m"
+print_head  "install systemd" 
 cp $script_path/${component_shp}.systemd /etc/systemd/system/${component_shp}.service &>>${log_path}
 func_exit $?
 
-echo -e " \e[31m >>>>> start mysql <<<<< \e[0m"
+print_head  "start mysql" 
 systemctl daemon-reload &>>${log_path}
 func_exit $?
 systemctl enable ${component_shp}  &>>${log_path}
