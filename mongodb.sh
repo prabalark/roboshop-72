c1652669f2b77bd8e74ec85758deb10c59e1907c
prabala <radhakrishnaprabala8@gmail.com>
2024-06-10 08:19:58 +0530

    mongodb.sh funver1

--- mongodb.sh
+++ mongodb.sh
@@ -1,20 +1,26 @@
 source common.sh
 
-echo  -e "\e[32m >>>>>>>>>Copy-repo<<<<<<<<<<<< \e [0m"
+
+func_mongodb() { 
+print_head "Copy-repo"
 cp /home/centos/roboshop-72/mongo.repo /etc/yum.repos.d/mongo.repo &>>${log_path}
 func_exit $?
 
-echo  -e "\e[32m >>>>>>>>>install mongo <<<<<<<<<<<< \e [0m"
+print_head "install mongo" 
 dnf install mongodb-org -y &>>${log_path}
 func_exit $?
 
-echo  -e "\e[32m >>>>>>>>>update listen <<<<<<<<<<<< \e [0m"
+print_head "update listen" 
 sed -i -e 's|127.0.0.1|0.0.0.0|' /etc/mongod.conf &>>${log_path}
 func_exit $?
 
-echo  -e "\e[32m >>>>>>>>>start mongodb <<<<<<<<<<<< \e [0m"
+print_head "start mongodb" 
 systemctl enable mongod &>>${log_path}
 func_exit $?
 systemctl restart mongod &>>${log_path}
 func_exit $?
+
 #Update listen address from 127.0.0.1 to 0.0.0.0 in /etc/mongod.conf
+}
+
+func_mongodb
