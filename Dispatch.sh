e5eb3cf63b66e82c7d2878bdf726cc757e61aa20
prabala <radhakrishnaprabala8@gmail.com>
2024-06-10 09:03:42 +0530

    Dispatch.sh funver1

--- Dispatch.sh
+++ Dispatch.sh
@@ -6,26 +6,26 @@ component_dis=dispatch
 
 func_discp(){
 
-echo -e " \e[32m >>>>> install golang <<<<< \e[0m "
+print_head   "install golang"   
 dnf install golang -y &>>${log_path}
 func_exit $?
 
-echo -e " \e[32m >>>>> useradd app <<<<< \e[0m "
+print_head   "useradd app"   
 useradd ${username} &>>${log_path}
 func_exit $?
 
-echo -e " \e[32m >>>>> downlaod app <<<<< \e[0m "
+print_head   "downlaod app"   
 mkdir /app &>>${log_path}
 func_exit $?
 curl -L -o /tmp/$component_dis.zip https://roboshop-artifacts.s3.amazonaws.com/$component_dis.zip &>>${log_path}
 func_exit $?
 
-echo -e " \e[32m >>>>> unzip app <<<<< \e[0m "
+print_head   "unzip app"   
 cd /app &>>${log_path}
 unzip /tmp/$component_dis.zip &>>${log_path}
 func_exit $?
 
-echo -e " \e[32m >>>>> install library <<<<< \e[0m "
+print_head   "install library"   
 cd /app &>>${log_path}
 go mod init dispatch &>>${log_path}
 func_exit $?
@@ -34,11 +34,11 @@ func_exit $?
 go build &>>${log_path}
 func_exit $?
 
-echo -e " \e[32m >>>>> install systemd <<<<< \e[0m "
+print_head   "install systemd"   
 cp ${script_path}/$component_dis.systemd  /etc/systemd/system/$component_dis.service &>>${log_path}
 func_exit $?
 
-echo -e " \e[32m >>>>> start  $component_dis <<<<< \e[0m "
+print_head   "start  $component_dis"   
 systemctl daemon-reload &>>${log_path}
 func_exit $?
 systemctl enable $component_dis &>>${log_path}
