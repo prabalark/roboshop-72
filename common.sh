f05443651a402b142e07f70c191d49d6e2ee7f66
prabala <radhakrishnaprabala8@gmail.com>
2024-08-06 08:43:25 +0530

    common.sh vr2

--- common.sh
+++ common.sh
@@ -43,16 +43,17 @@ func_systemd(){
 
 schema_laod_nodejs(){
   if [ "$schma_check" == "mongodb" ]; then  #double quotes compuslory
-echo -e "\e[32m >>>>>>> copy mongodb repo <<<<<<< \e[0m"
-cp ${script_path}/mongo.repo /etc/yum.repos.d/mongo.repo
+  echo -e "\e[32m >>>>>>> copy mongodb repo <<<<<<< \e[0m"
+  cp ${script_path}/mongo.repo /etc/yum.repos.d/mongo.repo
 
-echo -e "\e[32m >>>>>>> install mongodb client <<<<<<< \e[0m"
-dnf install mongodb-org-shell -y
+  echo -e "\e[32m >>>>>>> install mongodb client <<<<<<< \e[0m"
+  dnf install mongodb-org-shell -y
 
-echo -e "\e[32m >>>>>>> load schema <<<<<< \e[0m"
-mongo --host mongodb.devops72bat.online </app/schema/${component}.js
-fi
+  echo -e "\e[32m >>>>>>> load schema <<<<<< \e[0m"
+  mongo --host mongodb.devops72bat.online </app/schema/${component}.js
+  fi
 }
+
 
 func_exit(){
   if [ $1 -eq 0 ]; then
