7707bef5a72cd099b40ae6e41baa6efa3b72a62d
prabala <radhakrishnaprabala8@gmail.com>
2024-08-06 12:41:08 +0530

    shipping.sh vr1

--- shipping.sh
+++ shipping.sh
@@ -3,37 +3,30 @@ script_path=$(dirname "$script")
 source ${script_path}/common.sh
 mysqlpassword=$1
 
+component=shipping
+
  if [ -z "${mysqlpassword}" ]; then
     echo password is not given
     exit
  fi
 
-component=shipping
+fun_mysql(){
+  print_head  "install mysql"
+  dnf install mysql -y &>>${log_path}
+  func_exit $?
+
+  #RoboShop@1 --password as input
+  print_head  "load schema mysql"
+  mysql -h mysql.devops72bat.online -uroot -p${mysqlpassword} < /app/schema/${component}.sql &>>${log_path}
+  func_exit $?
+}
 
 func_shipping(){
 print_head  "install maven" 
 dnf install maven -y &>>${log_path}
 func_exit $?
 
-print_head "create /app"
-id ${username} &>>${log_path}
-if [ $? -ne 0 ]; then
- useradd ${username} &>>${log_path}
-fi
-func_exit $?
-
-rm -rf /app  &>>${log_path}
-func_exit $? # bcz re-run of code some time through error
-mkdir /app  &>>${log_path}
-func_exit $?
-
-print_head  "downlaod ${component}" 
-curl -L -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip &>>${log_path}
-func_exit $?
-cd /app &>>${log_path}
-
-print_head  "unzip ${component}" 
-unzip /tmp/${component}.zip &>>${log_path}
+func_prereq &>>${log_path} # in ansible also same
 func_exit $?
 
 print_head  "install library" 
@@ -43,14 +36,8 @@ func_exit $?
 mv target/shipping-1.0.jar shipping.jar &>>${log_path}
 func_exit $?
 
-print_head  "install mysql" 
-dnf install mysql -y &>>${log_path}
-func_exit $?
-
-#RoboShop@1 --password as input
-print_head  "load schema mysql" 
-mysql -h mysql.devops72bat.online -uroot -p${mysqlpassword} < /app/schema/${component}.sql &>>${log_path}
-func_exit $?
+fun_mysql &>>${log_path}
+&>>${log_path}
 
 func_systemd &>>${log_path}
 func_exit $?
