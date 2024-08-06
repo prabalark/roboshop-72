bd1ca167bc723d466b75701235ba18f36814ae84
prabala <radhakrishnaprabala8@gmail.com>
2024-08-06 13:09:05 +0530

    payment.sh vr1

--- payment.sh
+++ payment.sh
@@ -8,7 +8,7 @@ if [ -z "${rabbitm1_lg_password}" ]; then
   exit
 fi
 
-component_pay=payment
+component=payment
 
 func_payment(){
 print_head  "install python"   
@@ -24,13 +24,13 @@ rm -rf /app &>>${log_path}
 mkdir /app &>>${log_path}
 func_exit $?
 
-print_head  "download ${component_pay}.zip"   
-curl -L -o /tmp/${component_pay}.zip https://roboshop-artifacts.s3.amazonaws.com/${component_pay}.zip &>>${log_path}
+print_head  "download ${component}.zip"   
+curl -L -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip &>>${log_path}
 func_exit $?
 
 print_head  "unzip app"   
 cd /app &>>${log_path}
-unzip /tmp/${component_pay}.zip &>>${log_path}
+unzip /tmp/${component}.zip &>>${log_path}
 func_exit $?
 
 print_head  "install library"   
@@ -40,16 +40,16 @@ func_exit $?
 
 #roboshop123
 print_head  "install systemd"   
-sed -i -e "s|rabbitm1_lg_password2|${rabbitm1_lg_password}|" ${script_path}/${component_pay}.systemd &>>${log_path}
+sed -i -e "s|rabbitm1_lg_password2|${rabbitm1_lg_password}|" ${script_path}/${component}.systemd &>>${log_path}
   # only if password in systemd we have to do sed
 func_exit $?
-cp ${script_path}/${component_pay}.systemd  /etc/systemd/system/${component_pay}.service &>>${log_path}
+cp ${script_path}/${component}.systemd  /etc/systemd/system/${component}.service &>>${log_path}
 
-print_head  "start ${component_pay} systemd"   
+print_head  "start ${component} systemd"   
 systemctl daemon-reload  &>>${log_path}
 func_exit $?
-systemctl enable ${component_pay} &>>${log_path}
-systemctl restart ${component_pay} &>>${log_path}
+systemctl enable ${component} &>>${log_path}
+systemctl restart ${component} &>>${log_path}
 func_exit $?
 }
 
